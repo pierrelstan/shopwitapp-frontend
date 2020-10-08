bee3537d850d53b70b93a783ba86ce898c7465d6
Stanley <stanleypl75@gmail.com>
2020-10-08 12:39:17 -0400

    :art: Configure react-router, redux & material-ui

--- src/index.js
+++ src/index.js
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from '@material-ui/core/styles';
 import store, { persistor } from './redux/store/store';
 import './index.css';
 import App from './App';
+import { theme } from './Theme/theme';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-      {/* </PersistGate> */}
-    </Provider>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
