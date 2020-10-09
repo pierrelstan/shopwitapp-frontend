36572a408ff808a0b1fe426c04ce38dea1413ea1
Stanley <stanleypl75@gmail.com>
2020-10-09 19:02:00 -0400

    :sparkles: Create the Home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome Stanley and Happy Coding!</h1>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome Stanley and Happy Coding!</h1>
+    </div>
+  );
+};
+export default Home;
