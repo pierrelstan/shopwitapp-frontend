bd70d15e7d687f9652cf89573bbdad55f0eaf50a
Stanley <stanleypl75@gmail.com>
2020-10-28 07:17:40 -0400

    :sparkles: Add reducer scrollValues to combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import alert from './alert';
+import scrollValues from './scrollValues';
 const allReducers = combineReducers({
   auth,
   alert,
+  scrollValues,
 });
 export default allReducers;
