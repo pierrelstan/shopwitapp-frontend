d65a8b391a0d4053430d74020007d2502a65cf2b
Stanley <stanleypl75@gmail.com>
2020-10-28 07:15:20 -0400

    :sparkles: Create reducer scrollValues

---
+++ src/redux/reducers/scrollValues.js
@@ -0,0 +1,16 @@
+import { ADD_SCROLL_VALUES } from '../actions/types';
+let initialState = {
+  values: 0,
+};
+export default (state = initialState, action) => {
+  const { type, values } = action;
+  switch (type) {
+    case ADD_SCROLL_VALUES:
+      return {
+        ...state,
+        values: values,
+      };
+    default:
+      return state;
+  }
+};
