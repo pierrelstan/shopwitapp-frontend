132b284ed12f35ba126e788a32d058517b0c6fbf
Stanley <stanleypl75@gmail.com>
2020-11-02 17:36:36 -0400

    :sparkles: Add items and item in combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,14 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import alert from './alert';
 import scrollValues from './scrollValues';
+import items from './items';
+import item from './item';
+
 const allReducers = combineReducers({
   auth,
   alert,
   scrollValues,
+  items,
+  item,
 });
 export default allReducers;
