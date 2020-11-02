2756fb13ca7c5c2fad2f35fb9344f133c53046c4
Stanley <stanleypl75@gmail.com>
2020-11-02 17:27:04 -0400

    :sparkles: Major updates
    Add connect, withRouter, MenuNavigation,PopularProducts,Wrapper,MostRated,NewArrivals,MarginTop and create props ScrollNumber

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import MenuNavigation from '../components/MenuNavigation';
+import PopularProducts from '../components/PopularProducts';
+import Wrapper from '../components/Wrapper';
+import MostRated from '../components/MostRated';
+import NewArrivals from '../components/NewArrivals';
+import MarginTop from '../components/MarginTop';
 
-const Home = () => {
+const Home = (props) => {
   return (
     <div>
-      <Hero />
+      <Hero ScrollNumber={props.values} />
+      <Wrapper>
+        <div>
+          <MarginTop />
+          <MenuNavigation />
+          <MarginTop />
+          <PopularProducts />
+          <MarginTop />
+          <MostRated />
+          <MarginTop />
+        </div>
+      </Wrapper>
+      <NewArrivals ScrollNumber={props.values} />
     </div>
   );
 };
-export default Home;
+const mapStateToProps = (state) => ({
+  // items: state.items,
+  // carts: state.RootCarts.allCarts,
+  // favorites: state.toggleFavorite.favoriteFilm,
+  values: state.scrollValues.values,
+});
+
+export default withRouter(connect(mapStateToProps, {})(Home));
