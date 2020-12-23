730fc172e38ae4227c18b73dcba4939e04b694a6
Stanley <stanleypl75@gmail.com>
2020-12-23 09:58:29 -0400

    :lipstick: Create SickButton

---
+++ src/Styles/SickButton.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const SickButton = styled.button`
+  background: #cb436b;
+  color: white;
+  font-weight: 500;
+  border: 0;
+  border-radius: 0;
+  text-transform: uppercase;
+  cursor: pointer;
+  font-size: 2rem;
+  padding: 0.8rem 1.5rem;
+  display: inline-block;
+  transition: all 0.5s;
+  &[disabled] {
+    opacity: 0.5;
+  }
+`;
+
+export default SickButton;
