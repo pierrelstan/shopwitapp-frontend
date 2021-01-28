f20b9902398a79c7489b7b31935570ab45ef0ba6
Stanley <stanleypl75@gmail.com>
2021-01-28 16:38:59 -0400

    :sparkles: Update allFavorites

--- src/redux/reducers/favorites.js
+++ src/redux/reducers/favorites.js
@@ -26,7 +26,7 @@ export default function (state = initialState, action) {
     case FETCH_FAVORITES:
       return {
         ...state,
-        allFavorites: action.payload,
+        allFavorites: [...action.payload],
         isLoaded: true,
       };
 
