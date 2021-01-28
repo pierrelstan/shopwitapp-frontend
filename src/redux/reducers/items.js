f6462cdb0cf728a9259e68825e684cb66fe20f3c
Stanley <stanleypl75@gmail.com>
2021-01-28 16:25:10 -0400

    :sparkles: Add CREATE_ITEM FETCH_ITEMS_BY_USER_ID,
      FETCH_ITEMS_BY_USER_ID_FAILED,

--- src/redux/reducers/items.js
+++ src/redux/reducers/items.js
@@ -3,23 +3,51 @@ import {
   NEW_ITEM,
   REMOVE_ITEM_BY_ID,
   FETCH_ITEM_BY_ID,
+  CREATE_ITEM,
+  FETCH_ITEMS_BY_USER_ID,
+  FETCH_ITEMS_BY_USER_ID_FAILED,
+  FETCH_LAST_PRODUCTS,
 } from '../actions/types';
 
 const initialState = {
   items: [],
+  myProducts: [],
+  lastProducts: [],
+  createItem: {},
   isLoaded: false,
   error: null,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case CREATE_ITEM:
+      return {
+        ...state,
+        createItem: action.payload,
+        isLoaded: true,
+        error: null,
+      };
+
     case FETCH_ITEMS:
       return {
         ...state,
-        items: action.payload.data,
+        items: action.payload,
         isLoaded: true,
       };
-
+    case FETCH_LAST_PRODUCTS:
+      return {
+        ...state,
+        lastProducts: action.payload,
+        isLoaded: true,
+      };
+    case FETCH_ITEMS_BY_USER_ID:
+      return {
+        ...state,
+        myProducts: action.payload,
+        isLoaded: true,
+      };
+    case FETCH_ITEMS_BY_USER_ID_FAILED:
+      return state;
     case NEW_ITEM:
       return {
         ...state,
