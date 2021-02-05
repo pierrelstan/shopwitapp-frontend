c879f4c06395dec0c502f946ed9aea7ee11b610c
Stanley <stanleypl75@gmail.com>
2021-02-04 20:01:04 -0400

    :sparkles: Create LOGIN_START & LOGIN_FAILURE

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,130 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Paper } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+import { openCart } from '../redux/actions/openCart';
+import {
+  allCarts,
+  removeCart,
+  updateCart,
+} from '../redux/actions/ItemsActions';
+import { setAlert } from '../redux/actions/alert';
 import CartStyles from '../Styles/CartStyles';
 import CloseButton from '../Styles/CloseButton';
 import Supreme from '../Styles/Supreme';
+// import formatMoney from './formatMoney';
+import SickButton from '../Styles/SickButton';
+import { CalculatePriceTotal } from '../utils/CalcutateCartTotal';
+function Cart({
+  open,
+  openCart,
+  carts,
+  allCarts,
+  updateCart,
+  setAlert,
+  userId,
+  removeCart,
+}) {
+  const [total, setTotal] = React.useState();
 
-export default function Cart() {
+  const [cartImage, setCartImage] = React.useState();
+
+  React.useEffect(() => {
+    CalculatePriceTotal(carts, setCartImage, setTotal);
+  }, [carts]);
+
+  const handleChange = (id) => async (e) => {
+    let number = {
+      quantity: e.target.value,
+    };
+    await updateCart(id, number);
+  };
+
+  const handleDeleteCart = (id) => {
+    removeCart(id);
+  };
+  const handleClickOpenCart = () => {
+    openCart(false);
+  };
+
   return (
-    <CartStyles open>
+    <CartStyles open={open}>
       <header>
-        <Supreme>Your cart</Supreme>
-        <CloseButton title='close'>&times;</CloseButton>
-        <p>You have items in your cart</p>
+        <Supreme>ShopWit</Supreme>
+        <CloseButton onClick={handleClickOpenCart}>&times;</CloseButton>
+        {<h1>You have {carts.length} items in your cart</h1>}
       </header>
+      <div>
+        <ul>
+          {carts &&
+            carts.map((cart) => (
+              <Paper
+                style={{
+                  margin: '10px',
+                }}
+                key={cart._id}
+              >
+                <li>
+                  {cart.item === null ? (
+                    <div>
+                      <h4> This item has been removed by the owner</h4>
+
+                      <button onClick={() => handleDeleteCart(cart._id)}>
+                        Delete
+                      </button>
+                    </div>
+                  ) : (
+                    <>
+                      <h2> {cart.item.title}</h2>
+                      <img src={cart.item.imageUrl} alt={cart.item.title} />
+                      <h2>Quantity</h2>
+                      <input
+                        className='cart_input'
+                        type='number'
+                        name='quantity'
+                        defaultValue={cart.quantity}
+                        onChange={handleChange(cart._id)}
+                        min='1'
+                        max='250'
+                      />
+                      <h2>
+                        Price <span> ${cart.item.price} </span>
+                      </h2>
+
+                      <button onClick={() => handleDeleteCart(cart._id)}>
+                        Delete
+                      </button>
+                    </>
+                  )}
+                </li>
+              </Paper>
+            ))}
+        </ul>
+      </div>
       <footer>
-        <p>100.0$</p>
-        <button>Check</button>
+        <p
+          style={{
+            color: '#cb436b',
+          }}
+        >
+          {total}$
+        </p>
+        <SickButton>Check</SickButton>
       </footer>
     </CartStyles>
   );
 }
+const mapStateToProps = (state) => ({
+  carts: state.carts.allCarts,
+  userId: state.auth.user._id,
+  openCart: state.openCart.cart,
+});
+export default withRouter(
+  connect(mapStateToProps, {
+    removeCart,
+    updateCart,
+    setAlert,
+    allCarts,
+    openCart,
+  })(Cart),
+);
