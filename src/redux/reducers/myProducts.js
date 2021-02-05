c563c1d6afb717bcf259ae04739c69e0d9185906
Stanley <stanleypl75@gmail.com>
2021-02-04 21:34:41 -0400

    :sparkles: Create myProducts reducer

---
+++ src/redux/reducers/myProducts.js
@@ -0,0 +1,23 @@
+import { isEmptyArray } from 'formik';
+import { FETCH_ITEMS_BY_USER_ID } from '../actions/types';
+
+const initialState = {
+  myProducts: [],
+  isLoaded: false,
+  error: null,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case FETCH_ITEMS_BY_USER_ID:
+      return {
+        ...state,
+        myProducts: [...action.payload],
+        isLoaded: true,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+}
