9af2c524a4e6d5daa7b6875bcee1669a37ad5b36
Stanley <stanleypl75@gmail.com>
2021-02-04 22:42:53 -0400

    :recycle: Turn to React Hooks

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,131 +1,208 @@
-import React, { Component } from 'react';
-import { withRouter, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { register } from '../redux/actions/auth';
-import { setAlert } from '../redux/actions/alert';
+import React from 'react'
+import { withRouter, Redirect, useHistory } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import { register } from '../redux/actions/auth'
+import { setAlert } from '../redux/actions/alert'
+import {
+  Avatar,
+  Button,
+  CircularProgress,
+  Container,
+  CssBaseline,
+  TextField,
+  Typography,
+} from '@material-ui/core'
 
-class SignUp extends Component {
-  state = {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(3),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.secondaryColor.danger,
+    textAlign: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  centered: {
+    display: 'grid',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}))
+
+const SignUp = ({ register, isAuthenticated, setAlert, active }) => {
+  const [User, setUser] = React.useState({
     firstname: 'Stanley',
     lastname: 'Pierre-Louis',
     email: 'stanley01@gmail.com',
     password: '123456',
     confirmPassword: '123456',
-  };
+  })
+  let history = useHistory()
+  const classes = useStyles()
   //   fix error binding
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
+  const [open, setOpen] = React.useState(false)
 
-  handleSubmit = (e) => {
-    const {
-      email,
-      password,
-      confirmPassword,
-      firstname,
-      lastname,
-    } = this.state;
-    console.log(this.state);
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      this.props.setAlert('Password do not match', 'danger');
+  const handleToggle = () => {
+    if (active) {
+      setOpen(!open)
     } else {
-      this.props.register(
-        {
-          email,
-          password,
-          confirmPassword,
-          firstname,
-          lastname,
-        },
-        this.props,
-      );
+      setOpen(!open)
     }
-  };
+  }
+  const handleChange = (e) => {
+    setUser({ ...User, [e.target.name]: e.target.value })
+  }
+  React.useEffect(() => {
+    if (active) {
+      history.push('/')
+      setUser({})
+    }
+    if (alert.length === 1) {
+      setOpen(false)
+    }
+  }, [active, history])
 
-  render() {
-    if (this.props.isAuthenticated) {
-      return <Redirect to='/' />;
+  const handleSubmit = (e) => {
+    const { email, password, confirmPassword, firstname, lastname } = User
+
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setAlert('Password do not match', 'danger')
+    } else {
+      register({
+        email,
+        password,
+        confirmPassword,
+        firstname,
+        lastname,
+      })
     }
-    const {
-      email,
-      password,
-      confirmPassword,
-      firstname,
-      lastname,
-    } = this.state;
-    return (
-      <div className='container'>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>Fisrtname:</label>
-            <input
-              type='text'
-              name='firstname'
-              ref='firstname'
-              value={firstname}
-              onChange={this.handleChange}
-              placeholder=' Enter your firstname'
-            />
-          </div>
-          <div>
-            <label>Lastname:</label>
-            <input
-              type='text'
-              name='lastname'
-              ref='lastname'
-              value={lastname}
-              onChange={this.handleChange}
-              placeholder=' Enter your lastname'
-            />
-          </div>
-          <div>
-            <label>Email</label>
-            <input
-              type='text'
-              name='email'
-              ref='name'
-              value={email}
-              onChange={this.handleChange}
-              placeholder=' Enter the email'
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              type='password'
-              name='password'
-              ref='password'
-              value={password}
-              onChange={this.handleChange}
-              placeholder='Enter the password'
-            />
-          </div>
-          <div>
-            <label>Confirm Password</label>
-            <input
-              type='password'
-              name='confirmPassword'
-              value={confirmPassword}
-              onChange={this.handleChange}
-              placeholder='Enter the password'
-            />
-          </div>
-          <input type='submit' value='Submit' />
+  }
+
+  if (isAuthenticated) {
+    return <Redirect to="/" />
+  }
+  const { email, password, confirmPassword, firstname, lastname } = User
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          SignUp
+        </Typography>
+        <form onSubmit={handleSubmit} className={classes.form} noValidate>
+          <TextField
+            fullWidth
+            // error={Errors}
+            variant="outlined"
+            margin="normal"
+            type="text"
+            label="First Name"
+            name="firstname"
+            value={firstname}
+            onChange={handleChange}
+            placeholder=" Enter your firstname"
+          />
+
+          <TextField
+            fullWidth
+            // error={Errors}
+            label="Last Name"
+            variant="outlined"
+            margin="normal"
+            type="text"
+            name="lastname"
+            value={lastname}
+            onChange={handleChange}
+            placeholder=" Enter your lastname"
+          />
+
+          <TextField
+            fullWidth
+            // error={Errors}
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            type="text"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            placeholder=" Enter the email"
+          />
+
+          <TextField
+            fullWidth
+            // error={Errors}
+            label="Password"
+            variant="outlined"
+            margin="normal"
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            placeholder="Enter the password"
+          />
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Confirm password"
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={handleChange}
+            placeholder="Enter the password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            value="Submit"
+            className={classes.submit}
+            onClick={handleToggle}
+          >
+            {' '}
+            {!open ? (
+              'SignUp'
+            ) : (
+              <CircularProgress color="inherit" thickness={2.3} size={30} />
+            )}
+          </Button>
         </form>
       </div>
-    );
-  }
+    </Container>
+  )
 }
 
 const mapStateToProps = (state) => ({
   register: state.register,
   isAuthenticated: state.auth.isAuthenticated,
   active: state.auth.user.active,
-});
+})
 
 export default withRouter(
-  connect(mapStateToProps, { register, setAlert })(SignUp),
-);
+  connect(mapStateToProps, { register, setAlert })(SignUp)
+)
