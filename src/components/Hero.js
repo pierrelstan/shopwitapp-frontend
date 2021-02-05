8af0e799a917c7baf22a1575661340ec5688b1d0
Stanley <stanleypl75@gmail.com>
2021-02-04 22:02:59 -0400

    Add Image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,9 @@ import React from 'react';
 import clsx from 'clsx';
 import { Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Link as RouterLink } from 'react-router-dom';
 import styled, { keyframes, css } from 'styled-components';
+import image from '../utils/images/logo1.png';
 
 const RigthToLeftAnimations = keyframes`
   0%{
@@ -28,45 +27,75 @@ const BottomToptAnimations = keyframes`
     transform : translateY(0px)
   }
 `;
-
+const LeftToRightAnimations = keyframes`
+0%{
+  opacity:1;
+transform : translateX(-40px)
+}
+100% {
+  opacity: 1
+  transform : translateX(0px)
+}
+`;
 const ContenText = styled.div`
   opacity: 0px;
-  position: 'absolute';
+  position: relative;
+  /* width: 100vw; */
+
   animation: ${(props) =>
     props.ScrollNumber <= 8
       ? css`
           ${BottomToptAnimations} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1)
         `
       : ''};
+  @media (max-width: 320px) {
+    animation: ${(props) =>
+      props.ScrollNumber <= 8
+        ? css`
+            ${LeftToRightAnimations} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1)
+          `
+        : ''};
+  }
 `;
 
 const ImageContainer = styled.div`
+  position: relative;
   animation: ${(props) =>
     props.ScrollNumber <= 8
       ? css`
           ${RigthToLeftAnimations} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1)
         `
       : ''};
+  /* @media (max-width: 320px) {
+    animation: ${(props) =>
+    props.ScrollNumber <= 8
+      ? css`
+          ${LeftToRightAnimations} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1)
+        `
+      : ''};
+  } */
 `;
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr ',
+    justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
     gap: '40px',
     marginBottom: '40px',
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-    },
-    [theme.breakpoints.between('990', '1170')]: {
-      display: 'flex',
+    height: '100vh',
+    [theme.breakpoints.down('xs')]: {
+      // gridTemplateColumns: '130px 180px ',
+      gridTemplateColumns: '109px 180px ',
       height: 'auto',
+      gap: '0px',
     },
+    // [theme.breakpoints.between('990', '1170')]: {
+    //   display: 'flex',
+    //   height: 'auto',
+    // },
   },
 
   imageHero: {
@@ -74,6 +103,10 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: '455px',
     height: 'auto',
     objectFit: 'cover',
+    [theme.breakpoints.down('xs')]: {
+      width: '227px',
+      marginTop: '72px',
+    },
   },
 
   textContainer: {
@@ -90,22 +123,23 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 'bold',
     color: '#333',
     [theme.breakpoints.down('xs')]: {
-      fontSize: '18px',
+      fontSize: '32px',
+      textAlign: 'left',
     },
   },
 
   h1: {
     fontSize: '18px',
-    letterSpacing: '15px',
+    letterSpacing: '8px',
     color: '#333',
     margin: 0,
     [theme.breakpoints.down('xs')]: {
-      fontSize: '18px',
+      display: 'none',
     },
   },
 
   paperContainer: {
-    backgroundImage: `url(https://res.cloudinary.com/stanley/image/upload/v1587764936/header-img_azo9gj.webp)`,
+    backgroundImage: `/images/logo1.png`,
   },
   containerText: {
     display: 'flex',
@@ -114,67 +148,79 @@ const useStyles = makeStyles((theme) => ({
   button: {
     color: '#fff',
     border: 'none',
+    padding: '10px 40px',
     backgroundColor: '#cb436b',
-    transition: '1s cubic-bezier(0.2, 0.8,0.2, 1)',
     '&:hover': {
       backgroundColor: '#cb436b',
       color: '#fff',
-      transform: 'translateY(-4px)',
       boxShadow:
         '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
     },
-    [theme.breakpoints.down('sm')]: {
-      margin: '#30px',
+    [theme.breakpoints.down('xs')]: {
+      padding: '0px 6px',
+    },
+  },
+  left: {
+    textAlign: 'center',
+    [theme.breakpoints.down('xs')]: {
+      textAlign: 'left',
+    },
+  },
+  center: {
+    display: 'flex',
+    alignSelf: 'center',
+    marginBottom: '30px',
+    [theme.breakpoints.down('xs')]: {
+      alignSelf: 'flex-start',
     },
   },
 }));
 
-export default function Hero(props) {
+function Hero(props) {
   const classes = useStyles();
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div className={classes.container}>
-      <ContenText {...props}>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '10px',
-          }}
-        >
-          <Typography variant='h1' className={classes.h1}>
-            #NEW SUMMER COLLECTION 2020
-          </Typography>
-          <Typography variant='h4' className={classes.h4}>
-            NEW ARRIVALS
-          </Typography>
-
+      <div className={classes.center}>
+        <ContenText {...props}>
           <div
             style={{
-              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '10px',
             }}
           >
-            <Button
-              size={`${matches ? 'small' : 'large'}`}
-              variant='outlined'
-              className={clsx(classes.margin, classes.button)}
-              component={RouterLink}
-              to='/shop'
-            >
-              SHOW NOW
-            </Button>
+            <Typography variant='h1' className={classes.h1}>
+              #NEW SUMMER COLLECTION 2021
+            </Typography>
+            <Typography variant='h4' className={classes.h4}>
+              NEW ARRIVALS
+            </Typography>
+
+            <div className={classes.left}>
+              <Button
+                variant='outlined'
+                className={clsx(classes.margin, classes.button)}
+                component={RouterLink}
+                to='/shop'
+              >
+                SHOW NOW
+              </Button>
+            </div>
           </div>
-        </div>
-      </ContenText>
+        </ContenText>
+      </div>
+
       <ImageContainer {...props}>
         <img
-          src='https://res.cloudinary.com/stanley/image/upload/v1604165151/home_t7eo9k.png'
+          src={image}
           alt='hero_image'
+          width='640'
+          height='360'
           className={classes.imageHero}
         />
       </ImageContainer>
     </div>
   );
 }
+export default Hero;
