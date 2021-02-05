e1f7133ab2b70f6e8b789b8fb80074f18cfc9540
Stanley <stanleypl75@gmail.com>
2021-02-04 21:15:25 -0400

    Add lastProducts & myProducts

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,25 +1,29 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import alert from './alert';
-import scrollValues from './scrollValues';
-import items from './items';
-import item from './item';
-import pages from './pages';
-import openCart from './openCart';
-import carts from './RootCarts';
-import favorites from './favorites';
-import openFavoritesAndClosing from './OpenAndCloseFavorites';
+import { combineReducers } from 'redux'
+import auth from './auth'
+import alert from './alert'
+import scrollValues from './scrollValues'
+import items from './items'
+import item from './item'
+import pages from './pages'
+import openCart from './openCart'
+import carts from './RootCarts'
+import favorites from './favorites'
+import myProducts from './myProducts'
+import lastProducts from './lastProducts'
+import openFavoritesAndClosing from './OpenAndCloseFavorites'
 
 const allReducers = combineReducers({
+  lastProducts,
   auth,
   alert,
   scrollValues,
   items,
+  myProducts,
   item,
   pages,
   openCart,
   carts,
   favorites,
   openFavoritesAndClosing,
-});
-export default allReducers;
+})
+export default allReducers
