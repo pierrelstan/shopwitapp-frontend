88258a2a0af720e623fa748d11e461d44797ee23
Stanley <stanleypl75@gmail.com>
2021-02-04 22:26:16 -0400

    Display a quantity of items per page

--- src/components/ShopAll.js
+++ src/components/ShopAll.js
@@ -3,15 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia';
-import { Box } from '@material-ui/core';
+import { Box, CircularProgress } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-import { useDispatch } from 'react-redux';
-import { Rating } from '@material-ui/lab';
 import Link from '@material-ui/core/Link';
 import { Button } from '@material-ui/core';
+import FavoriteSharpIcon from '@material-ui/icons/FavoriteSharp';
+import RemoveShoppingCartSharpIcon from '@material-ui/icons/RemoveShoppingCartSharp';
 import { Link as RouterLink } from 'react-router-dom';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
@@ -19,62 +16,105 @@ import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { fetchItems, addToCart, allCarts } from '../redux/actions/ItemsActions';
-
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import {
+  addToCart,
+  allCarts,
+  removeCart,
+  fetchItems,
+} from '../redux/actions/ItemsActions';
+import { addToFavorites, removeFavorites } from '../redux/actions/favorites';
+import Footer from './Footer';
 const useStyles = makeStyles((theme) => ({
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+  },
+  container: {
+    width: 'auto',
+    margin: 0,
+    [theme.breakpoints.between('1200', '1368')]: {
+      width: '1080px',
+      margin: '0 auto',
+    },
   },
   containerItems: {
     display: 'grid',
-    gridTemplateColumns: '347px 347px 347px ',
-    // gridTemplateRows: '1fr 1fr',
-    gap: '20px',
-    // margin: 0,
+    gridTemplateColumns: '320px 320px 320px ',
+    justifyItems: 'center',
+    gap: '42px',
+    margin: '20px',
+    [theme.breakpoints.down('sm')]: {
+      justifyContent: 'center',
+      gridTemplateColumns: '1fr 1fr',
+    },
+    [theme.breakpoints.down('md')]: {
+      justifyContent: 'center',
+      gridTemplateColumns: '1fr 1fr',
+    },
     [theme.breakpoints.down('xs')]: {
-      gridTemplateColumns: '247px',
+      justifyContent: 'center',
+      gridTemplateColumns: '320px',
     },
-
-    [theme.breakpoints.only('md')]: {
-      gridTemplateColumns: '300px 300px',
-      margin: 0,
+    [theme.breakpoints.between('1200', '1368')]: {
+      gridTemplateColumns: '320px 320px 320px',
     },
-    [theme.breakpoints.between('sm', 'md1030')]: {
-      gridTemplateColumns: '300px 300px',
-      margin: 0,
-    },
-    [theme.breakpoints.only('sm')]: {
-      gridTemplateColumns: '250px 250px',
-      margin: 0,
-    },
-    // [theme.breakpoints.up('sm')]: {
-    //   gridTemplateColumns: '300px 300px ',
-    // },
   },
   textLink: {
     textDecoration: 'none',
     color: '#333',
+    '&:hover': {
+      textDecoration: 'none',
+    },
   },
-
-  main: {
-    display: 'flex',
-    justifyContent: 'center',
-    // backgroundColor: '#00000005',
-    // padding: '30px',
+  imageCard: {
+    // width: '100%',
+    // height: 'auto',
+    maxWidth: '317px',
+    verticalAlign: 'middle',
+    position: 'relative',
+    top: '-88px',
+    objectFit: 'cover',
+    margin: 0,
+    padding: 0,
+    width: '322px',
+    height: '304px',
+    zIndex: 1,
+    // objectFit: 'cover',
+    '&:hover': {
+      boxShadow: 'none',
+    },
+    [theme.breakpoints.between('sm', 'lg')]: {
+      objectFit: 'cover',
+    },
+    [theme.breakpoints.only('xs')]: {
+      objectFit: 'contain',
+      position: 'relative',
+      top: '-89px',
+    },
+    [theme.breakpoints.up('sm')]: {
+      objectFit: 'cover',
+    },
     [theme.breakpoints.down('sm')]: {
-      margin: ' 30px',
-      justifyContent: 'center',
+      objectFit: 'cover',
+      top: '-180px',
+    },
+    [theme.breakpoints.down('md')]: {
+      objectFit: 'cover',
+      top: '-80px',
     },
   },
   card: {
-    // width: '307px',
+    maxWidth: '327px',
     height: '337px',
-
     position: 'relative',
     borderRadius: '24px',
     transition: '0.5s ease-in-out ',
     zIndex: 1,
+    // background: 'linear-gradient(to bottom, rgb(0 0 0 / 4%),rgb(0 0 0 / 67%))',
+    [theme.breakpoints.down('xs')]: {
+      width: '277px',
+      height: '296px',
+    },
     boxShadow:
       ' 0 5px 10px rgba(154,160,185,.05), 0 15px 40px rgba(166,173,201,.2)',
     '&:hover': {
@@ -97,9 +137,9 @@ const useStyles = makeStyles((theme) => ({
       zIndex: 2,
       transition: '0.5s all',
       opacity: 0,
-      // background: 'linear-gradient(to bottom, rgba(0,0,0,0.6),rgba(0,0,0,0.2))',
+      borderRadius: '24px',
       background:
-        'linear-gradient(to bottom, rgb(0 0 0 / 44%),rgb(0 0 0 / 67%))',
+        'linear-gradient(to bottom, rgb(0 0 0 / 14%),rgb(0 0 0 / 87%))',
     },
     '&:hover $price': {
       background: '#fff',
@@ -157,42 +197,14 @@ const useStyles = makeStyles((theme) => ({
       opacity: 1,
       transform: 'translateY(0px)',
     },
-
-    // [theme.breakpoints.down('xs')]: {
-    //   height: '240px',
-    // },
   },
-  imageCard: {
-    width: '100%',
-    height: 'auto',
-    MaxWidth: '347px',
-    position: 'relative',
-    top: '-175px',
-    objectFit: 'cover',
-    '&:hover': {
-      boxShadow: 'none',
-    },
-    [theme.breakpoints.between('sm', 'lg')]: {
-      objectFit: 'cover',
-    },
-    [theme.breakpoints.only('xs')]: {
-      objectFit: 'contain',
-      // width: '145px',
-      position: 'relative',
-      top: '-89px',
-      // left: '58px',
-    },
-    [theme.breakpoints.up('sm')]: {
-      objectFit: 'cover',
-    },
-    [theme.breakpoints.down('sm')]: {
-      objectFit: 'cover',
-      top: '-180px',
-    },
-    [theme.breakpoints.down('sm')]: {
-      objectFit: 'cover',
-      top: '-130px',
-    },
+  button: {
+    color: '#fff !important',
+    objectFit: 'filled',
+    margin: '20px',
+    padding: '10px 20px',
+    borderRadius: '5px',
+    transition: '1s cubic-bezier(0.2, 0.8, 0.2, 1)',
   },
   removeHover: {
     backgroundColor: 'none',
@@ -223,119 +235,188 @@ const useStyles = makeStyles((theme) => ({
   containerButton: {
     display: 'flex',
     justifyContent: 'space-around',
-
-    [theme.breakpoints.down('xs')]: {
-      position: 'relative',
-      top: '120px',
-    },
-    [theme.breakpoints.down('380')]: {
-      position: 'relative',
-      top: '70px',
-    },
-    [theme.breakpoints.only('sm')]: {
-      position: 'relative',
-      top: '-40px',
-    },
+    position: 'relative',
+    width: '322px',
+    top: '200px',
   },
 }));
-function ShopAll(props) {
+function ShopAll({
+  addToCart,
+  removeCart,
+  removeFavorites,
+  itemsPerPages,
+  favorites,
+  carts,
+  addToFavorites,
+  fethcItems,
+}) {
   const classes = useStyles();
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down('xs'));
+  const [Favs, setFavs] = React.useState([]);
+  const [Carts, setCarts] = React.useState([]);
 
-  const [value, setValue] = React.useState(2);
+  React.useEffect(() => {
+    const favs = favorites.allFavorites;
+    const Carrts = carts.allCarts;
+    setFavs([...favs]);
+    setCarts([...Carrts]);
+  }, [carts.allCarts, favorites.allFavorites]);
 
-  const dispatch = useDispatch();
-  // console.log(props);
+  // React.useEffect(() => {
+  //   fetchItems()
+  // }, [])
+
+  const handleRemoveFavorite = React.useCallback(
+    (id) => {
+      let fav = Favs.find((item) => item.item._id === id);
+      removeFavorites(fav._id);
+    },
+    [Favs, removeFavorites],
+  );
+
+  const hanldeRemoveCart = React.useCallback(
+    (id) => {
+      let cart = Carts.filter((item) => item.item._id === id);
+      removeCart(cart[0]._id);
+    },
+    [Carts, removeCart],
+  );
+
+  const handleAddCart = React.useCallback(
+    (id) => {
+      addToCart(id);
+    },
+    [addToCart],
+  );
+
+  const handleAddFavorites = React.useCallback(
+    (id) => {
+      addToFavorites(id);
+    },
+    [addToFavorites],
+  );
+
+  const DisplayFavoriteIcon = (id) => (
+    <div>
+      {Favs.findIndex((item) => item.item._id === id) !== -1 ? (
+        <FavoriteSharpIcon
+          onClick={() => handleRemoveFavorite(id)}
+          style={{
+            fontSize: 62,
+            color: '#cb436b',
+          }}
+        />
+      ) : (
+        <FavoriteBorderIcon
+          style={{
+            fontSize: 62,
+          }}
+          onClick={() => handleAddFavorites(id)}
+        />
+      )}
+    </div>
+  );
+
+  const DisplayShoppingIcon = (id) => {
+    if (Carts.findIndex((item) => item.item._id === id) !== -1) {
+      return (
+        <AddShoppingCartIcon
+          onClick={() => hanldeRemoveCart(id)}
+          style={{
+            fontSize: 62,
+            color: '#4BB543',
+          }}
+        />
+      );
+    } else {
+      return (
+        <ShoppingCartIcon
+          style={{
+            fontSize: 62,
+          }}
+          onClick={() => handleAddCart(id)}
+        />
+      );
+    }
+  };
+
   return (
     <div className={classes.main}>
       <Grid className={classes.containerItems}>
-        {props.itemsPerPages.map((data) => (
-          <Card
-            className={classes.card}
-            key={data.title}
-            elevation={matches ? 1 : 0}
-          >
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'flex-end',
-                padding: '20px',
-              }}
+        {itemsPerPages &&
+          itemsPerPages.map((data) => (
+            <Card
+              className={classes.card}
+              key={data.title}
+              elevation={matches ? 1 : 0}
             >
-              <Typography component='span' className={classes.price}>
-                ${data.price}
-              </Typography>
-            </div>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                  padding: '20px',
+                }}
+              >
+                <Typography component='span' className={classes.price}>
+                  ${data.price}
+                </Typography>
+              </div>
 
-            <img
-              alt={data.title}
-              // height='50'
-              src={data.imageUrl}
-              title={data.title}
-              className={classes.imageCard}
-            />
-            <Link
-              component={RouterLink}
-              to={`/item/${data._id}`}
-              className={classes.textLink}
-            >
-              <CardContent>
+              <img
+                alt={data.title}
+                src={data.imageUrl}
+                title={data.title}
+                className={classes.imageCard}
+              />
+              <Link href={`/item/${data._id}`} className={classes.textLink}>
+                <CardContent>
+                  <div>
+                    <h1>{data.title}</h1>
+                  </div>
+                  <p>{data.description}</p>
+                </CardContent>
+              </Link>
+              <div className={classes.containerButton}>
                 <div>
-                  <h1>{data.title}</h1>
+                  <Box>
+                    <div className={classes.textLink}>
+                      <Button className={classes.button}>
+                        {DisplayFavoriteIcon(data._id)}
+                      </Button>
+                    </div>
+                  </Box>
                 </div>
-                <p>{data.description}</p>
-              </CardContent>
-            </Link>
-            <div className={classes.containerButton}>
-              <div>
-                <Box>
-                  <div className={classes.textLink}>
-                    <Button className={classes.button}>
-                      <FavoriteBorderIcon
-                        style={{
-                          fontSize: 62,
-                        }}
-                      />
-                    </Button>
-                  </div>
-                </Box>
+                <div>
+                  <Box>
+                    <div>
+                      <Button className={classes.button}>
+                        {DisplayShoppingIcon(data._id)}
+                      </Button>
+                    </div>
+                  </Box>
+                </div>
               </div>
-              <div>
-                <Box>
-                  <div
-                    onClick={() => {
-                      props.addToCart(data._id);
-                      console.log(data._id);
-                    }}
-                  >
-                    <Button className={classes.button}>
-                      <AddShoppingCartIcon
-                        style={{
-                          fontSize: 62,
-                        }}
-                      />
-                    </Button>
-                  </div>
-                </Box>
-              </div>
-            </div>
-          </Card>
-        ))}
+            </Card>
+          ))}
       </Grid>
     </div>
   );
 }
 
 const mapStateToProps = (state) => ({
-  items: state.items,
-  carts: state.RootCarts.allCarts,
+  itemsPerPages: state.pages.itemsPerPages,
+  carts: state.carts,
+  favorites: state.favorites,
 });
 
 export default withRouter(
   connect(mapStateToProps, {
-    fetchItems,
     addToCart,
     allCarts,
+    fetchItems,
+    removeCart,
+    removeFavorites,
+    addToFavorites,
   })(ShopAll),
 );
