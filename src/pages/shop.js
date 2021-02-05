34d9fd20ce772f35ed2d945299ec71c9fde25807
Stanley <stanleypl75@gmail.com>
2021-02-04 22:45:43 -0400

    Updates

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,37 +1,70 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Pagination from '@material-ui/lab/Pagination';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
-import NewArrivals from '../components/NewArrivals';
+import axios from 'axios';
 import { Container } from '@material-ui/core';
-import PaginationControlled from '../components/Pagination';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import NewArrivals from '../components/NewArrivals';
+import { pagesControlled } from '../redux/actions/pages';
+import { fetchItems } from '../redux/actions/ItemsActions';
+import ShopAll from '../components/ShopAll';
+import { Paper } from '@material-ui/core';
+import SearchFromPagination from '../components/SearchFromPagination';
 import ScrollOnTop from '../components/ScrollOnTop';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > * + *': {
-      marginTop: theme.spacing(2),
+      display: 'flex',
+      // justifyContent: 'center',
+      margin: '40px',
+    },
+  },
+  pagination: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '40px',
+  },
+
+  search: {
+    justifySelf: 'end',
+    [theme.breakpoints.down('xs')]: {
+      gridColumnStart: 1,
+      gridRowStart: 1,
+      justifySelf: 'start',
+    },
+    [theme.breakpoints.down('sm')]: {
+      justifySelf: 'start',
     },
   },
 }));
 
-function Shop(props) {
+function Shop({ itemsPerPages, items, pagesControlled }) {
   const classes = useStyles();
+  const [page, setPage] = React.useState(1);
 
-  let history = useHistory();
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
-  function handleClick(event) {
-    console.info('You clicked a breadcrumb.');
-    history.push('/');
-  }
+  useEffect(() => {
+    const cancelTokenSource = axios.CancelToken.source();
+    pagesControlled(page, cancelTokenSource.token);
+    return () => {
+      cancelTokenSource.cancel();
+    };
+  }, [page, pagesControlled]);
 
   return (
     <div>
-      <ScrollOnTop />
       <NewArrivals />
       <Container>
+        <ScrollOnTop />
+
         <Breadcrumbs
           maxItems={2}
           aria-label='breadcrumb'
@@ -44,7 +77,7 @@ function Shop(props) {
             style={{
               cursor: 'pointer',
             }}
-            onClick={handleClick}
+            // onClick={handleClick}
           >
             Home
           </Link>
@@ -65,10 +98,47 @@ function Shop(props) {
         >
           Shop All
         </Typography>
-        <PaginationControlled />
+
+        <div>
+          <div className={classes.root}>
+            <Pagination
+              className={classes.pagination}
+              count={
+                (items.length / 10) % 2 === 0
+                  ? Math.round(items.length / 10 + 1)
+                  : Math.floor(items.length / 10 + 1)
+              }
+              page={page}
+              onChange={handleChange}
+              style={{ color: '#cb436b' }}
+            />
+          </div>
+
+          <ShopAll page={page} />
+
+          <div className={classes.root}>
+            <Typography>Page: {page}</Typography>
+            <Pagination
+              className={classes.pagination}
+              count={
+                (items.length / 10 + 1) % 2 === 0
+                  ? Math.round(items.length / 10 + 1)
+                  : Math.floor(items.length / 10 + 1)
+              }
+              page={page}
+              onChange={handleChange}
+              style={{ color: '#cb436b' }}
+            />
+          </div>
+        </div>
       </Container>
     </div>
   );
 }
+const mapStateToProps = (state) => ({
+  itemsPerPages: state.pages.itemsPerPages,
+  items: state.items.items,
+  page: state.pages.page,
+});
 
-export default Shop;
+export default withRouter(connect(mapStateToProps, { pagesControlled })(Shop));
