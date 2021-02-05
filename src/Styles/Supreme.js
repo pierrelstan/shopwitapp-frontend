68f9cc4770f3aa633d45410c9161f1dab3f653dd
Stanley <stanleypl75@gmail.com>
2021-02-04 21:42:52 -0400

    :lipstick: Updates

--- src/Styles/Supreme.js
+++ src/Styles/Supreme.js
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 
 const Supreme = styled.h3`
-  background: ${(props) => props.theme.red};
-  color: white;
+  color: #454754;
   display: inline-block;
+  border: 2px solid #454754;
   padding: 4px 5px;
-  transform: skew(-3deg);
+  text-transform: uppercase;
+  /* transform: skew(-3deg); */
   margin: 0;
   font-size: 4rem;
+  @media (max-width: 430px) {
+    font-size: 22px;
+  }
+  @media (max-width: 860px) {
+    font-size: 22px;
+  }
 `;
 
 export default Supreme;
