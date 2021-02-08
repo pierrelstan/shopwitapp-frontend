56d79230ce4b08b215a793e83d813f0a9f2f7ac0
Stanley <stanleypl75@gmail.com>
2021-02-08 19:06:31 -0400

    Implement  useSeletor & useDispatch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React, { Fragment } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  withRouter,
-} from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 import Home from './pages/home';
 import Navbar from './components/Navbar';
 import AuthLinks from './components/AuthLinks';
 import GuestLinks from './components/GuestLinks';
-import { loadUser, logout } from './redux/actions/auth';
-import {
-  allCarts,
-  fetchItemsByUserId,
-  fetchItems,
-} from './redux/actions/ItemsActions';
-import axios from 'axios';
+import { loadUser } from './redux/actions/auth';
 import { allFavorites } from './redux/actions/favorites';
 import Login from './pages/Login';
 import Profile from './pages/profile';
@@ -26,7 +15,6 @@ import NoMatch from './components/NoMatch';
 import Alert from './components/Alert';
 import SignUp from './pages/SignUp';
 import ProtectedRoutes from './components/protectedRoutes/Protected';
-import store from './redux/store/store';
 import Sell from './pages/Sell';
 import EditItem from './pages/EditItem';
 import Item from './pages/Item';
@@ -40,11 +28,11 @@ import MyProducts from './pages/MyProducts';
 import Orders from './pages/orders';
 import Dashboard from './pages/dashboard';
 import Footer from './components/Footer';
+import { allCarts, fetchItemsByUserId } from './redux/actions/ItemsActions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    minHeight: '100vh',
     flexDirection: 'column',
   },
   paper: {
@@ -73,130 +61,64 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function App({
-  allCarts,
-  userId,
-  allFavorites,
-  fetchItemsByUserId,
-  token,
-  loading,
-  active,
-  isAuthenticated,
-}) {
+function App() {
   const classes = useStyles();
-  let fetchallCarts = React.useRef(() => {});
-  let fetchallFavorites = React.useRef(() => {});
-  let fetchallItemsByUserId = React.useRef(() => {});
+  const dispatch = useDispatch();
 
-  fetchallCarts.current = () => {
-    const cancelTokenSource = axios.CancelToken.source();
-    allCarts(userId, cancelTokenSource.token);
-  };
-
-  fetchallFavorites.current = () => {
-    const cancelTokenSource = axios.CancelToken.source();
-    allFavorites(userId, cancelTokenSource.token);
-  };
-  fetchallItemsByUserId.current = () => {
-    const cancelTokenSource = axios.CancelToken.source();
-    fetchItemsByUserId(userId, cancelTokenSource.token);
-  };
+  const { token, active } = useSelector((state) => ({
+    token: state.auth.token,
+    active: state.auth.user ? state.auth.user.active : false,
+  }));
 
   React.useEffect(() => {
     if (token !== null) {
-      store.dispatch(loadUser());
-      fetchallCarts.current();
-      fetchallFavorites.current();
-      fetchallItemsByUserId.current();
+      dispatch(loadUser());
+      dispatch(allFavorites());
+      dispatch(allCarts());
+      dispatch(fetchItemsByUserId());
     }
-
-    return () => {
-      const cancelTokenSource = axios.CancelToken.source();
-      cancelTokenSource.cancel();
-    };
-  }, [token]);
+  }, [dispatch, token]);
 
   return (
-    <React.Fragment>
-      <div className={classes.root}>
-        <Router>
-          {/* <ScrollOnT /> */}
-          <div className={classes.main}>
-            <Navbar>
-              {active && <AuthLinks />}
-              {!active && <GuestLinks />}
-            </Navbar>
+    <div className={classes.root}>
+      <Router>
+        <Navbar>
+          {active && <AuthLinks />}
+          {!active && <GuestLinks />}
+        </Navbar>
+        <Alert />
+        <div className={classes.main}>
+          <Switch>
+            <Route exact path='/' render={(props) => <Home {...props} />} />
+            <Route path='/login' render={(props) => <Login {...props} />} />
+            <Route
+              exact
+              path='/register'
+              render={(props) => <SignUp {...props} />}
+            />
+            <ProtectedRoutes path='/profile' component={Profile} />
+            <ProtectedRoutes path='/orders' component={Orders} />
+            <ProtectedRoutes path='/dashboard' component={Dashboard} />
+            <ProtectedRoutes path='/item/new' component={Sell} />
+            <ProtectedRoutes path='/item/update/:id' component={EditItem} />
+            <ProtectedRoutes path='/item/:id' component={Item} />
+            <Route path='/newpassword/:id' component={NewPassword} />
+            <ProtectedRoutes path='/myproducts' component={MyProducts} />
+            <Route path='/login' component={Login} />
+            <Route path='/resetpassword' component={ResetPassword} />
+            <Route path='/shop' component={Shop} />
+            <Route path='/woman' component={Woman} />
+            <Route path='/men' component={Men} />
+            <Route path='/shoes' component={Shoes} />
+            <Route path='*' component={NoMatch} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
 
-            <Alert />
-
-            <Switch>
-              <Route exact path='/' render={(props) => <Home {...props} />} />
-              <Route
-                exact
-                path='/login'
-                render={(props) => <Login {...props} />}
-              />
-              <Route
-                exact
-                path='/register'
-                render={(props) => <SignUp {...props} />}
-              />
-              <ProtectedRoutes exact path='/profile' component={Profile} />
-              <ProtectedRoutes exact path='/orders' component={Orders} />
-              <ProtectedRoutes exact path='/dashboard' component={Dashboard} />
-              <ProtectedRoutes exact path='/item/new' component={Sell} />
-              <ProtectedRoutes
-                exact
-                path='/item/update/:id'
-                component={EditItem}
-              />
-              <ProtectedRoutes exact path='/item/:id' component={Item} />
-              <Route exact path='/newpassword/:id' component={NewPassword} />
-              <ProtectedRoutes
-                exact
-                path='/myproducts'
-                component={MyProducts}
-              />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/resetpassword' component={ResetPassword} />
-              <Route exact path='/shop' component={Shop} />
-              <Route exact path='/woman' component={Woman} />
-              <Route exact path='/men' component={Men} />
-              <Route exact path='/shoes' component={Shoes} />
-              <Route path='*' component={NoMatch} />
-            </Switch>
-          </div>
-        </Router>
-        {/* <MobileNavbar /> */}
-        <Footer />
-      </div>
-    </React.Fragment>
+      {/* <MobileNavbar /> */}
+    </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  const {
-    auth: { isAuthenticated },
-    loading,
-    logout,
-  } = state;
-  return {
-    isAuthenticated: isAuthenticated,
-    loading: loading,
-    active: state.auth.user ? state.auth.user.active : false,
-    logout: logout,
-    items: state.items,
-    userId: state.auth.user ? state.auth.user._id : 0,
-    token: state.auth.token,
-  };
-};
-export default withRouter(
-  connect(mapStateToProps, {
-    allCarts,
-    loadUser,
-    logout,
-    fetchItems,
-    allFavorites,
-    fetchItemsByUserId,
-  })(App),
-);
+export default App;
