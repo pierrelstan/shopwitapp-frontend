f0471ef42c1810b261faacbc2be023d7fa09c158
Stanley <stanleypl75@gmail.com>
2021-02-10 17:33:55 -0400

    :sparkles: Implement Backend_url for producion & development

---
+++ src/config.js
@@ -0,0 +1,7 @@
+let BACKEND_URL = 'http://localhost:4000/';
+
+if (process.env.NODE_ENV === 'production') {
+  BACKEND_URL = 'https://mern-shopwit-backend.herokuapp.com/';
+}
+
+export { BACKEND_URL };
