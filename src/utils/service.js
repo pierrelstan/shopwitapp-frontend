8d1f56a5f4646668466609afb4f739761384f520
Stanley <stanleypl75@gmail.com>
2021-02-11 20:58:11 -0400

    Create services Api

---
+++ src/utils/service.js
@@ -0,0 +1,65 @@
+import axiosService from './axiosService';
+
+class WebAPI {
+  static Log_in = async (data) => {
+    return axiosService.post(`/api/auth/login`, data);
+  };
+
+  // static requestPasswordReset = async (data) => {
+  //   return axiosService.post(`/request-password-reset`, data);
+  // };
+
+  // static resetPassword = async (data) => {
+  //   return axiosService.post(`/reset-password`, data);
+  // };
+
+  static getProfile = async (id) => {
+    return axiosService.get(`/api/auth/me/${id}`);
+  };
+
+  static signUp = async (data) => {
+    return axiosService.post(`/api/auth/signup`, data);
+  };
+
+  static editProfile = async (USER_ID, User) => {
+    return axiosService.put(`/api/auth/user/${USER_ID}/edit`, User);
+  };
+
+  // static subscribeToPlan = (payload) => {
+  //   return axiosService.post(`/user_plans`, {
+  //     plan_id: payload.plan_id,
+  //   });
+  // };
+
+  // static getPlans = () => {
+  //   return axiosService.get(`/plans`);
+  // };
+
+  // static getMovies = (page) => {
+  //   return axiosService.get(`/movies?page=${page}`);
+  // };
+
+  // static getMovieDetails = (id) => {
+  //   return axiosService.get(`/movies/${id}`);
+  // };
+
+  // static getFavoriteMovies = (userId) => {
+  //   return axiosService.get(`/favorites-by-user?user_id=${userId}`);
+  // };
+
+  // static addFavoriteMovies = (user_id, movie_id) => {
+  //   return axiosService.post(`/favorites`, {
+  //     user_id,
+  //     movie_id,
+  //   });
+  // };
+
+  // static removeFavoriteMovies = (userId, movieId) => {
+  //   return axiosService({
+  //     method: 'DELETE',
+  //     url: `/favorites?user_id=${userId}&movie_id=${movieId}`,
+  //   });
+  // };
+}
+
+export default WebAPI;
