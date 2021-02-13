66243dbeba4147c3bdda630cc9e9d89f6926e250
Stanley <stanleypl75@gmail.com>
2021-02-12 21:57:31 -0400

    Remove actions carts

--- src/redux/actions/ItemsActions.js
+++ src/redux/actions/ItemsActions.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 import store from '../store/store';
 import {
@@ -7,39 +6,26 @@ import {
   CLEAR_SEARCH_ITEMS,
   FETCH_ITEM_BY_ID,
   REMOVE_ITEM_BY_ID,
-  ADD_TO_CART_BY_ID,
-  FAILED_ADD_TO_CART_BY_ID,
-  FETCH_CARTS,
-  REMOVE_CART_BY_ID,
-  UPDATE_CART,
-  SET_ALERT,
   CREATE_ITEM,
   CREATE_ITEM_FAILED,
   UPDATE_ITEM,
   UPDATE_ITEM_FAIL,
   FETCH_ITEMS_BY_USER_ID,
-  FETCH_LAST_PRODUCTS,
   DELETE_ITEM_FAIL,
   FETCH_LAST_PRODUCTS_START,
   FETCH_LAST_PRODUCTS_SUCCESS,
 } from './types';
 import { setAlert } from './alert';
+import axiosService from '../../utils/axiosService';
+import WebAPI from '../../utils/service';
 
 export const fetchLastProducts = (cancelToken) => async (dispatch) => {
-  let config = {
-    headers: {
-      'Content-Type': 'application/json',
-      // cancelToken,
-    },
-  };
   try {
     dispatch({
       type: FETCH_LAST_PRODUCTS_START,
     });
-    let items = await axios.get(
-      'http://10.0.0.5:4000/api/item/lastproducts',
-      config,
-    );
+    let items = await WebAPI.fetchLastProducts();
+
     dispatch({
       type: FETCH_LAST_PRODUCTS_SUCCESS,
       lastProducts: items.data,
@@ -47,13 +33,8 @@ export const fetchLastProducts = (cancelToken) => async (dispatch) => {
   } catch (err) {}
 };
 export const fetchItems = () => async (dispatch, getState) => {
-  let config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
   try {
-    let items = await axios.get('http://10.0.0.5:4000/api/item/all', config);
+    let items = await WebAPI.fetchItems();
     dispatch({
       type: FETCH_ITEMS,
       payload: items.data,
@@ -66,12 +47,11 @@ export const fetchItems = () => async (dispatch, getState) => {
 };
 
 export const CreateItem = (product, history) => (dispatch) => {
-  axios
-    .post('http://10.0.0.5:4000/api/item/new', product, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+  const token = store.getState().auth.token;
+  const { user } = jwtDecode(token);
+  let USER_ID = user.userId;
+
+  WebAPI.createItem(product, USER_ID)
     .then((response) => {
       dispatch(fetchLastProducts());
       dispatch(fetchItemsByUserId());
@@ -95,8 +75,8 @@ export const CreateItem = (product, history) => (dispatch) => {
 };
 export const searchItems = (data) => (dispatch) => {
   const { search } = data;
-  axios
-    .get(`http://10.0.0.5:4000/api/item/s/search?`, {
+  axiosService
+    .get(`/api/item/s/search?`, {
       params: {
         title: `${search}`,
       },
@@ -139,8 +119,8 @@ export const fetchItemById = (id) => (dispatch) => {
     },
   };
 
-  axios
-    .get(`http://10.0.0.5:4000/api/item/${id}`, config)
+  axiosService
+    .get(`/api/item/${id}`, config)
     .then((item) =>
       dispatch({
         type: FETCH_ITEM_BY_ID,
@@ -167,8 +147,8 @@ export const removeItemById = (id) => (dispatch) => {
     },
   };
 
-  axios
-    .delete(`http://10.0.0.5:4000/api/item/${id}`, config)
+  axiosService
+    .delete(`/api/item/${id}`, config)
     .then((data) => {
       dispatch(fetchLastProducts());
       dispatch(fetchItems());
@@ -195,17 +175,9 @@ export const fetchItemsByUserId = (cancelToken) => async (dispatch) => {
   const token = store.getState().auth.token;
   const { user } = jwtDecode(token);
   let USER_ID = user.userId;
-  let config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    cancelToken: cancelToken, // 2nd step
-  };
+
   try {
-    let itemsByUserId = await axios.get(
-      `http://10.0.0.5:4000/api/item/user/items/${USER_ID} `,
-      config,
-    );
+    let itemsByUserId = await WebAPI.fetchItemsByUserId(USER_ID);
 
     await dispatch({
       type: FETCH_ITEMS_BY_USER_ID,
@@ -217,25 +189,19 @@ export const fetchItemsByUserId = (cancelToken) => async (dispatch) => {
   }
 };
 
-export const updateItem = (id, state) => async (dispatch) => {
-  const token = store.getState().auth.token;
-  const { user } = jwtDecode(token);
-  let USER_ID = user.userId;
-  let config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
+export const updateItem = (id, state, history) => async (dispatch) => {
   try {
-    let data = await axios.put(`http://10.0.0.5:4000/api/item/${id}`, state, {
-      config,
-    });
+    const token = store.getState().auth.token;
+    const { user } = jwtDecode(token);
+    let USER_ID = user.userId;
+    let data = await WebAPI.updateItem(id, state, USER_ID);
+
     dispatch({
       type: UPDATE_ITEM,
       payload: data.data,
       isLoaded: true,
     });
+    history.push(`/myproducts/${USER_ID}`);
     dispatch(setAlert('Updated cart successfully!', 'success'));
     dispatch(fetchItems());
     dispatch(fetchItemsByUserId(USER_ID));
@@ -248,114 +214,4 @@ export const updateItem = (id, state) => async (dispatch) => {
       type: UPDATE_ITEM_FAIL,
     });
   }
-};
-
-export const addToCart = (id) => async (dispatch) => {
-  try {
-    await axios
-      .post(`http://10.0.0.5:4000/api/item/add-to-cart/${id}`, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then((data) => {
-        dispatch({
-          type: ADD_TO_CART_BY_ID,
-          payload: data,
-          isLoaded: true,
-        });
-        dispatch(allCarts());
-        dispatch(setAlert('Add to cart successfully!', 'success'));
-      });
-  } catch (err) {
-    if (err.response.data.msg) {
-      dispatch(setAlert('Please login first!', 'warning'));
-    }
-    const errors = err.response.data.errors;
-    if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, 'warning')));
-    }
-    dispatch({
-      type: FAILED_ADD_TO_CART_BY_ID,
-    });
-  }
-};
-
-export const allCarts = (cancelToken) => async (dispatch) => {
-  const token = store.getState().auth.token;
-  const { user } = jwtDecode(token);
-  let USER_ID = user.userId;
-  let config = {
-    headers: {
-      'Content-Type': 'application/json',
-      cancelToken: cancelToken,
-    },
-  };
-  try {
-    let carts = await axios.get(
-      `http://10.0.0.5:4000/api/item/cart/${USER_ID}`,
-      config,
-    );
-    dispatch({
-      type: FETCH_CARTS,
-      payload: carts.data,
-      isLoaded: true,
-      error: null,
-    });
-  } catch (err) {
-    throw err;
-  }
-};
-export const updateCart = (id, number) => async (dispatch) => {
-  let config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: number,
-  };
-  try {
-    let res = await axios.put(
-      `http://10.0.0.5:4000/api/item/updateCart/${id}`,
-      number,
-      config,
-    );
-    dispatch({
-      type: UPDATE_CART,
-      payload: res.data,
-      isLoaded: true,
-    });
-    dispatch(setAlert('Updated cart successfully!', 'success'));
-    dispatch(allCarts());
-  } catch (error) {
-    dispatch({
-      type: SET_ALERT,
-    });
-    if (axios.isCancel(error)) {
-      console.log('request cancelled');
-    } else {
-      console.log('some other reason');
-      throw error;
-    }
-  }
-};
-export const removeCart = (id) => (dispatch) => {
-  let config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  axios
-    .post(`http://10.0.0.5:4000/api/item/removecart/${id}`, config)
-    .then((res) => {
-      dispatch({
-        type: REMOVE_CART_BY_ID,
-        payload: res.data,
-        isLoaded: true,
-        error: null,
-      });
-      dispatch(setAlert('Remove to cart successfully!', 'success'));
-      dispatch(allCarts());
-    })
-    .catch((err) => console.log(err));
 };
