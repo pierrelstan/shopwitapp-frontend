7b4a0fa712692fcf8514b62747d4037a0aac7813
Stanley <stanleypl75@gmail.com>
2021-02-16 12:36:19 -0400

    Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
