cfc43b5b8a3b252d3201f1adc7c6dd72b5013250
Stanley <stanleypl75@gmail.com>
2021-02-17 13:43:24 -0400

    Update links

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-let BACKEND_URL = 'http://localhost:4000/';
+let BACKEND_URL = 'http://localhost:4000';
 
 if (process.env.NODE_ENV === 'production') {
-  BACKEND_URL = 'https://mern-shopwit-backend.herokuapp.com/';
+  BACKEND_URL = 'https://mern-shopwit-backend.herokuapp.com';
 }
 
 export { BACKEND_URL };
