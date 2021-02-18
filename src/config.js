49e74c4cdf1c1e5bafa544391761cb0f9c99035f
Stanley <stanleypl75@gmail.com>
2021-02-18 13:18:35 -0400

    Update BACKEND_URL

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 let BACKEND_URL = 'http://localhost:4000';
 
 if (process.env.NODE_ENV === 'production') {
-  BACKEND_URL =
-    'https://pierrelstan.github.io/shopwitapp/https://mern-shopwit-backend.herokuapp.com';
+  BACKEND_URL = 'https://mern-shopwit-backend.herokuapp.com';
 }
 
 export { BACKEND_URL };
