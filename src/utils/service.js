d44a5c4721f9cbfd92e0ba66415a24b5c24281bb
pierrelstan <stanleypl75@gmail.com>
2021-05-30 20:37:06 -0400

    Update the links and parameters

--- src/utils/service.js
+++ src/utils/service.js
@@ -6,60 +6,60 @@ class WebAPI {
   };
 
   static fetchItems = async () => {
-    return axiosService.get(`api/item/all`);
+    return axiosService.get(`/api/items`);
   };
   static fetchItemsByUserId = async (USER_ID) => {
-    return axiosService.get(`/api/item/user/items/${USER_ID} `);
+    return axiosService.get(`/api/items/items/${USER_ID} `);
   };
   static fetchItemById = async (id) => {
-    return axiosService.get(`/api/item/${id}`);
+    return axiosService.get(`/api/items/${id}`);
   };
   static fetchLastProducts = async () => {
-    return axiosService.get(`/api/item/lastproducts`);
+    return axiosService.get('/api/items/lastproducts');
   };
   static updateItem = async (id, state, USER_ID) => {
-    return axiosService.put(`/api/item/${id}`, state);
+    return axiosService.put(`/api/items/${id}`, state);
   };
   static removeItemById = async (id, USER_ID) => {
-    return axiosService.post(`/api/item/item_id=${id}`, {
+    return axiosService.post(`/api/items/${id}`, {
       userId: USER_ID,
     });
   };
   static pagesControlled = async (page) => {
-    return axiosService.get(`api/item/page/${page}`);
+    return axiosService.get(`/api/items/page/${page}`);
   };
 
-  static createItem = async (product) => {
-    return axiosService.post('/api/item/new', product);
+  static createItem = async (formData) => {
+    return axiosService.post('/api/items/new', formData);
   };
 
   static addToCart = async (id, USER_ID) => {
-    return axiosService.post(`/api/add-to-cart/${id}`, {
+    return axiosService.post(`/api/carts/${id}`, {
       userId: USER_ID,
     });
   };
   static allCarts = async (USER_ID) => {
-    return axiosService.get(`/api/cart/${USER_ID}`);
+    return axiosService.get(`/api/carts/${USER_ID}`);
   };
   static updateCart = async (id, number, USER_ID) => {
-    return axiosService.put(`/api/updateCart/${id}`, {
+    return axiosService.put(`/api/carts/${id}`, {
       number,
       userId: USER_ID,
     });
   };
   static removeCart = async (id, USER_ID) => {
-    return axiosService.post(`/api/removecart/${id}`, { userId: USER_ID });
+    return axiosService.post(`/carts/${id}`, { userId: USER_ID });
   };
   static addToFavorites = async (id, USER_ID) => {
-    return axiosService.post(`/api/item/add-to-favorites/${id}`, {
+    return axiosService.post(`/api/favorites/${id}`, {
       userId: USER_ID,
     });
   };
   static allFavorites = async (USER_ID) => {
-    return axiosService.get(`/api/item/favorites/${USER_ID}`);
+    return axiosService.get(`/api/favorites/${USER_ID}`);
   };
   static removeFavorites = async (id, USER_ID) => {
-    return axiosService.post(`/api/item/removeFavorites/${id}`, {
+    return axiosService.post(`/api/favorites/remove/${id}`, {
       userId: USER_ID,
     });
   };
@@ -67,12 +67,12 @@ class WebAPI {
     return axiosService.get(`/api/auth/me/${id}`);
   };
 
-  static signUp = async (data) => {
-    return axiosService.post(`/api/auth/signup`, data);
+  static signUp = async (formData) => {
+    return axiosService.post(`/api/auth/signup`, formData);
   };
 
-  static editProfile = async (USER_ID, User) => {
-    return axiosService.put(`/api/auth/user/${USER_ID}/edit`, User);
+  static editProfile = async (USER_ID, formData) => {
+    return axiosService.put(`/api/auth/user/${USER_ID}/edit`, formData);
   };
 }
 
