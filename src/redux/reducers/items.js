39e711a95d5ab9a0bce29e2819d6a44e490505cd
Stanley <stanleypl75@gmail.com>
2021-07-18 17:39:49 -0400

    Update

--- src/redux/reducers/items.js
+++ src/redux/reducers/items.js
@@ -13,7 +13,7 @@ const initialState = {
   error: null,
 };
 
-export default function (state = initialState, action) { 
+export default function (state = initialState, action) {
   switch (action.type) {
     case CREATE_ITEM:
       return {
