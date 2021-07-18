ed9354be3a13a6add4e14c7370ff46900da5e4ba
Stanley <stanleypl75@gmail.com>
2021-07-18 17:18:56 -0400

    Add action Payments

---
+++ src/redux/actions/Payments.js
@@ -0,0 +1,21 @@
+import { PAYMENTS_START, PAYMENTS_SUCCESS, PAYMENTS_FAILED } from './types';
+import WebAPI from '../../utils/service';
+
+export const CreateOrderPayments =
+  (cartIds, Payment, id) => async (dispatch) => {
+    try {
+      dispatch({
+        type: PAYMENTS_START,
+      });
+      let items = await WebAPI.createOrderPayments(cartIds, Payment, id);
+
+      dispatch({
+        type: PAYMENTS_SUCCESS,
+        order: items.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: PAYMENTS_FAILED,
+      });
+    }
+  };
