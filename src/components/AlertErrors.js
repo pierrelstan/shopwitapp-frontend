30c5a5eb4ff7b74f88e2a117517e5be81bd28249
Stanley <stanleypl75@gmail.com>
2021-07-18 17:44:39 -0400

    Add AlertErrors

---
+++ src/components/AlertErrors.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { useSnackbar } from 'notistack';
+
+const AlertErrors = ({ alerts }) => {
+  const { enqueueSnackbar } = useSnackbar();
+  return (
+    <div
+      style={{
+        display: 'none',
+      }}
+    >
+      {alerts !== null &&
+        alerts.length > 0 &&
+        alerts.map((alert) => (
+          <div key={alert.id}>
+            {enqueueSnackbar(`${alert.msg}`, {
+              variant: `${alert.alertTypes}`,
+              preventDuplicate: true,
+              anchorOrigin: {
+                vertical: 'top',
+                horizontal: 'right',
+              },
+            })}
+          </div>
+        ))}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  alerts: state.alert,
+});
+export default connect(mapStateToProps)(
+  React.memo(AlertErrors, (prev, next) => {
+    if (prev === next.alerts) {
+      return true;
+    }
+  }),
+);
