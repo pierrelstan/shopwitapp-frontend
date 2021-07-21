4185fc3405840049d7978916e3a9f92e539faf16
Stanley <stanleypl75@gmail.com>
2021-07-21 07:03:17 -0400

    Add query

--- src/redux/actions/pages.js
+++ src/redux/actions/pages.js
@@ -1,11 +1,11 @@
 import { PAGES, LOADING_PAGE_START, LOADING_PAGE_SUCCESS } from './types';
 import WebAPI from '../../utils/service';
 
-export const pagesControlled = (page) => (dispatch) => {
+export const pagesControlled = (page, query) => (dispatch) => {
   dispatch({
     type: LOADING_PAGE_START,
   });
-  WebAPI.pagesControlled(page)
+  WebAPI.pagesControlled(page, query)
     .then((items) =>
       Promise.all([
         dispatch({
