7b1a202134269db28306e27be44dce087e3bd37e
Stanley <stanleypl75@gmail.com>
2021-08-07 17:19:06 -0400

    Remove all unused imports & add removeManyCarts

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -1,26 +1,11 @@
 import React, { useState } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  CardElement,
-  Elements,
-  useElements,
-  useStripe,
-  CardNumberElement,
-  CardExpiryElement,
-  CardCVCElement,
-  injectStripe,
-  StripeProvider,
-} from '@stripe/react-stripe-js';
-import Button from '@material-ui/core/Button';
+import { useDispatch } from 'react-redux';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import Dialog from '@material-ui/core/Dialog';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import './PaymentsStyle.css';
-import { Payment } from '@material-ui/icons';
 import { CreateOrderPayments } from '../redux/actions/Payments';
-
+import { removeManyCarts } from '../redux/actions/carts';
 const CARD_OPTIONS = {
   style: {
     base: {
@@ -101,7 +86,7 @@ const ResetButton = ({ onClick }) => (
   <button type='button' className='ResetButton' onClick={onClick}>
     <svg width='32px' height='32px' viewBox='0 0 32 32'>
       <path
-        fill='#FFF'
+        fill='#cb436b'
         d='M15,7.05492878 C10.5000495,7.55237307 7,11.3674463 7,16 C7,20.9705627 11.0294373,25 16,25 C20.9705627,25 25,20.9705627 25,16 C25,15.3627484 24.4834055,14.8461538 23.8461538,14.8461538 C23.2089022,14.8461538 22.6923077,15.3627484 22.6923077,16 C22.6923077,19.6960595 19.6960595,22.6923077 16,22.6923077 C12.3039405,22.6923077 9.30769231,19.6960595 9.30769231,16 C9.30769231,12.3039405 12.3039405,9.30769231 16,9.30769231 L16,12.0841673 C16,12.1800431 16.0275652,12.2738974 16.0794108,12.354546 C16.2287368,12.5868311 16.5380938,12.6540826 16.7703788,12.5047565 L22.3457501,8.92058924 L22.3457501,8.92058924 C22.4060014,8.88185624 22.4572275,8.83063012 22.4959605,8.7703788 C22.6452866,8.53809377 22.5780351,8.22873685 22.3457501,8.07941076 L22.3457501,8.07941076 L16.7703788,4.49524351 C16.6897301,4.44339794 16.5958758,4.41583275 16.5,4.41583275 C16.2238576,4.41583275 16,4.63969037 16,4.91583275 L16,7 L15,7 L15,7.05492878 Z M16,32 C7.163444,32 0,24.836556 0,16 C0,7.163444 7.163444,0 16,0 C24.836556,0 32,7.163444 32,16 C32,24.836556 24.836556,32 16,32 Z'
       />
     </svg>
@@ -130,6 +115,12 @@ const Payments = ({ total, cartIds }) => {
   const dispatch = useDispatch();
   const [open, setOpen] = React.useState(false);
 
+  React.useEffect(() => {
+    if (paymentMethod) {
+      dispatch(removeManyCarts(cartIds));
+    }
+  }, [cartIds, dispatch, paymentMethod]);
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -141,8 +132,6 @@ const Payments = ({ total, cartIds }) => {
     event.preventDefault();
 
     if (!stripe || !elements) {
-      // Stripe.js has not loaded yet. Make sure to disable
-      // form submission until Stripe.js has loaded.
       setProcessing(false);
       return;
     }
@@ -225,8 +214,11 @@ const Payments = ({ total, cartIds }) => {
                   color: '#333',
                 }}
               >
-                Thanks for trying Stripe Elements. No money was charged, but we
-                generated a PaymentMethod: {paymentMethod.id}
+                Thank you for your order. Your order number is <br />
+                {paymentMethod.id}.
+                <br /> We have emailed your order confirmation,
+                <br />
+                and will send you an update when your order has shipped.
               </div>
               <ResetButton onClick={reset} />
             </div>
@@ -345,7 +337,7 @@ const Payments = ({ total, cartIds }) => {
               <SubmitButton
                 processing={processing}
                 error={error}
-                disabled={!stripe}
+                disabled={!stripe || processing}
               >
                 Pay ${total}
               </SubmitButton>
