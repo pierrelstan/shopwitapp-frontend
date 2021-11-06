5306171350f2efff63850e4118b8ae4c13a79fdc
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-06 18:33:19 -0400

    Add minHeight

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
+    minHeight:'100vh',
   },
   paper: {
     padding: theme.spacing(2),
