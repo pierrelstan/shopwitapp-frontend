1b7c36cbb7ce4588f21614b972b75db0f7b10981
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-29 17:53:15 -0400

    Replace ModalDetails to ModalItemDetails

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,21 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link as RouterLink } from 'react-router-dom';
-import Link from '@material-ui/core/Link';
-import { Box, Button, Grid } from '@material-ui/core';
-import { addToCart, removeCart } from '../redux/actions/carts';
-import Card from '@material-ui/core/Card';
+import { Box, Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
-import {
-  removeFavorites,
-  allFavorites,
-  addToFavorites,
-} from '../redux/actions/favorites';
-import { DisplayFavoriteCartIcon } from '../components/DisplayFavoriteCartIcon';
-import DisplayShoppingCartIcon from '../components/DisplayShoppingCartIcon';
+import { removeFavorites , allFavorites } from '../redux/actions/favorites';
 import Titles from '../components/Titles';
+import MenuNavigation from '../components/MenuNavigation';
+import ModalItemDetails from "../components/ModalItemDetails"
 
 const useStyles = makeStyles((theme) => ({
   media: {
@@ -24,53 +17,43 @@ const useStyles = makeStyles((theme) => ({
   container: {
     width: 'auto',
     margin: 0,
-    [theme.breakpoints.between('1200', '1368')]: {
-      width: '1080px',
+    [theme.breakpoints.between('1200', '1440')]: {
+      width: '1440px',
       margin: '0 auto',
     },
   },
   containerItems: {
     display: 'grid',
-    gridTemplateColumns: '320px 320px 320px 320px',
+    minHeight: '400px',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr',
     justifyItems: 'center',
-    gap: '42px',
-    margin: '20px',
-    [theme.breakpoints.down('sm')]: {
-      justifyContent: 'center',
-      gridTemplateColumns: '1fr 1fr',
-    },
+    gap: '20px',
+    margin: '10px',
     [theme.breakpoints.down('md')]: {
       justifyContent: 'center',
+      gridTemplateColumns: '1fr 1fr 1fr',
+      gap: '10px',
+    },
+
+    [theme.breakpoints.down('665')]: {
+      justifyContent: 'center',
       gridTemplateColumns: '1fr 1fr',
+      gap: '12px',
     },
     [theme.breakpoints.down('xs')]: {
       justifyContent: 'center',
-      gridTemplateColumns: '320px',
-    },
-    [theme.breakpoints.between('1200', '1368')]: {
-      gridTemplateColumns: '320px 320px 320px',
-    },
-  },
-  textLink: {
-    textDecoration: 'none',
-    color: '#333',
-    '&:hover': {
-      textDecoration: 'none',
+      gridTemplateColumns: '1fr',
+      gap: '62px',
     },
   },
   imageCard: {
     maxWidth: '100%',
     objectFit: 'cover',
-    position: 'relative',
-    top: '-80px',
     margin: 0,
     padding: 0,
-    width: '322px',
-    height: '304px',
-    zIndex: 1,
-    '&:hover': {
-      boxShadow: 'none',
-    },
+    width: '320px',
+    height: '175px',
+
     [theme.breakpoints.between('sm', 'lg')]: {
       objectFit: 'cover',
     },
@@ -78,35 +61,17 @@ const useStyles = makeStyles((theme) => ({
       objectFit: 'cover',
       width: '100%',
     },
-    [theme.breakpoints.up('sm')]: {
-      objectFit: 'cover',
-    },
+
     [theme.breakpoints.down('sm')]: {
-      objectFit: 'cover',
-      top: '-180px',
-    },
-    [theme.breakpoints.down('md')]: {
-      objectFit: 'cover',
-      top: '-80px',
+      objectFit: 'contain',
     },
   },
   card: {
-    maxWidth: '327px',
-    height: '337px',
-    position: 'relative',
+    width: '200px',
+    height: '250px',
     borderRadius: '24px',
     transition: '0.5s ease-in-out ',
     padding: '10px',
-    zIndex: 1,
-    [theme.breakpoints.down('xs')]: {
-      width: '277px',
-      height: '296px',
-    },
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-      height: '336px',
-      maxWidth: '100%',
-    },
     boxShadow:
       ' 0 5px 10px rgba(154,160,185,.05), 0 15px 40px rgba(166,173,201,.2)',
     '&:hover': {
@@ -114,29 +79,6 @@ const useStyles = makeStyles((theme) => ({
       boxShadow:
         'linear-gradient(to bottom, rgba(0,176,155,1.8), rgba(150,201,61,2))',
       transform: 'translateY(20px)',
-    },
-    '&:hover:before': {
-      opacity: 1,
-    },
-
-    '&:before': {
-      content: '""',
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      display: 'block',
-      width: '100%',
-      height: '100%',
-      zIndex: 2,
-      transition: '0.5s all',
-      opacity: 0,
-      borderRadius: '24px',
-      background:
-        'linear-gradient(to bottom, rgb(0 0 0 / 35%),rgb(0 0 0 / 55%))',
-    },
-    '&:hover $price': {
-      background: '#fff',
-      color: '#333',
     },
 
     '& p': {
@@ -154,11 +96,6 @@ const useStyles = makeStyles((theme) => ({
       textAlign: 'center',
       backgroundColor: '#555',
     },
-    '&:hover p': {
-      display: 'block',
-      opacity: 1,
-      transform: 'translateY(0px)',
-    },
 
     '& h1': {
       textTransform: 'uppercase',
@@ -167,12 +104,8 @@ const useStyles = makeStyles((theme) => ({
       zIndex: 3,
       color: '#fff',
       opacity: 0,
-      transform: 'translateY(30px)',
+      transform: 'translateY(-30px)',
       transition: '0.5s all',
-    },
-    '&:hover h1': {
-      opacity: 1,
-      transform: 'translateY(0px)',
     },
     '& $button': {
       zIndex: 4,
@@ -185,6 +118,11 @@ const useStyles = makeStyles((theme) => ({
     '&:hover $button': {
       opacity: 1,
       transform: 'translateY(0px)',
+    },
+    '&:hover $price': {
+      background: '#fff',
+      color: '#d13c6f',
+      textDecoration: 'none'
     },
   },
   button: {
@@ -206,8 +144,8 @@ const useStyles = makeStyles((theme) => ({
     color: '#ffffff',
     borderRadius: '50%',
     backgroundColor: '#d13c6f',
-    width: '60px',
-    height: '60px',
+    width: '50px',
+    height: '50px',
     textAlign: 'center',
     display: 'flex',
     justifyContent: 'center',
@@ -216,6 +154,7 @@ const useStyles = makeStyles((theme) => ({
     zIndex: 2,
     boxShadow:
       '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+
   },
   centered: {
     display: 'flex',
@@ -228,60 +167,63 @@ const useStyles = makeStyles((theme) => ({
     top: '227px',
     width: '94%',
   },
+  TextLink: {
+    textDecoration: 'none! important',
+    cursor:'pointer',
+  },
+  ContainerPrice: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    position:'relative',
+    right:'auto',
+    zIndex: 3,
+  }
 }));
+
+
 
 function Favorites() {
   const dispatch = useDispatch();
   const classes = useStyles();
 
-  const { carts, favorites } = useSelector((state) => ({
+  const [open, setOpen] = useState(false);
+  const [idItem, setIdItem]= useState('');
+
+  const { item, favorites} = useSelector((state) => ({
     favorites: state.favorites.allFavorites,
-    userId: state.auth.user._id,
-    favOpenOrClose: state.openFavoritesAndClosing.open,
-    carts: state.carts,
+    loading: state.lastProducts.isLoadingLast10Products,
+    item: state.item,
   }));
 
-  const [state, setState] = React.useState([]);
+
+
+  useEffect(() => {
+    dispatch(allFavorites);
+}, [dispatch ,item]);
+
+  const handleClickOpen = (id) => {
+    setOpen(true);
+    if(id){
+  setIdItem(id);
+}
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleDeleteCart = (id) => {
     dispatch(removeFavorites(id));
   };
 
-  React.useEffect(() => {
-    (() => {
-      dispatch(allFavorites);
-    })();
-  }, [dispatch]);
-
-  React.useEffect(() => {
-    setState([...favorites]);
-  }, [favorites]);
-
-  const handleRemoveFavorite = (id) => {
-    let fav = favorites.find((item) => item.item._id === id);
-    dispatch(removeFavorites(fav._id));
-  };
-
-  const hanldeRemoveCart = (id) => {
-    let cart = carts.allCarts.filter((item) => item.item._id === id);
-    const { _id } = cart[0];
-    dispatch(removeCart(_id));
-  };
-
-  const handleAddCart = (id) => {
-    dispatch(addToCart(id));
-  };
-
-  const handleAddFavorites = (id) => {
-    dispatch(addToFavorites(id));
-  };
   return (
-    <Container maxWidth='xl'>
+    <Container maxWidth='lg'>
       <Titles>my Favorites</Titles>
       <Grid className={classes.containerItems}>
-        {state.map((cart) => (
-          <Card className={classes.card} key={cart.item.title} elevation={1}>
-            {cart.item === null ? (
+        {favorites.map((cart) => (
+          <Paper className={classes.card} key={cart.item.title} elevation={1}>
+            {
+              cart.item === null ? (
               <div>
                 <h4> This item has been removed by the owner</h4>
 
@@ -289,29 +231,14 @@ function Favorites() {
                   Delete
                 </button>
               </div>
-            ) : (
+            ) :
+             (
               <div>
-                <Link
-                  component={RouterLink}
-                  to={`/item/${cart.item._id}`}
-                  className={classes.textLink}
+                <Box
+                  onClick={()=>handleClickOpen(cart.item._id)}
+                  className={classes.TextLink}
                 >
-                  <Box>
-                    <div>
-                      <h1>{cart.item.title}</h1>
-                    </div>
-                    <p>
-                      <span>{cart.item.description}</span>
-                    </p>
-                  </Box>
-                </Link>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    padding: '20px',
-                  }}
-                >
+                <div className={classes.ContainerPrice}>
                   <Typography component='span' className={classes.price}>
                     ${cart.item.price}
                   </Typography>
@@ -323,42 +250,19 @@ function Favorites() {
                   title={cart.item.title}
                   className={classes.imageCard}
                 />
-
-                <div className={classes.containerButton}>
-                  <div>
-                    <Box boxShadow={0}>
-                      <div className={classes.textLink}>
-                        <Button className={classes.button}>
-                          <DisplayFavoriteCartIcon
-                            id={cart.item._id}
-                            Favs={favorites}
-                            handleAddFavorites={handleAddFavorites}
-                            handleRemoveFavorite={handleRemoveFavorite}
-                          />
-                        </Button>
-                      </div>
-                    </Box>
-                  </div>
-                  <div>
-                    <Box boxShadow={0}>
-                      <div className={classes.textLink}>
-                        <Button className={classes.button}>
-                          <DisplayShoppingCartIcon
-                            Carts={carts.allCarts}
-                            id={cart.item._id}
-                            handleAddCart={handleAddCart}
-                            hanldeRemoveCart={hanldeRemoveCart}
-                          />
-                        </Button>
-                      </div>
-                    </Box>
-                  </div>
-                </div>
+              </Box>
               </div>
-            )}
-          </Card>
+            )
+            }
+          </Paper>
         ))}
+      <ModalItemDetails
+      open={open}
+      handleClose={handleClose}
+      id={idItem}
+      />
       </Grid>
+      <MenuNavigation/>
     </Container>
   );
 }
