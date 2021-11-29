c1d85b648b084a1c4a1e2f902f5f2df2db67150f
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-29 16:54:34 -0400

    Configure useForm

--- src/pages/EditItem.js
+++ src/pages/EditItem.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, Controller } from 'react-hook-form';
 import { useParams, useHistory } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
@@ -12,7 +15,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormLabel from '@material-ui/core/FormLabel';
 import { updateItem, fetchItemById } from '../redux/actions/ItemsActions';
 import Titles from '../components/Titles';
-import { CircularProgress } from '@material-ui/core';
+import { CircularProgress, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -52,65 +55,47 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#e3e3e3',
   },
 }));
-const EditItem = ({ item, updateItem, userId, fetchItemById }) => {
-  const [Product, setProduct] = useState({
-    id: '',
-    title: '',
-    description: '',
-    price: '',
-    imageUrl: '',
-    quantityProducts: '',
-    gender: 'women',
-  });
+
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string()
+    .required('Title is required')
+    .min(6, 'Title must be at least 6 characters')
+    .max(10, 'Title must not exceed 10 characters'),
+    description: Yup.string()
+    .required('Description is required')
+    .min(10, 'Description must be at least 10 characters')
+    .max(100, 'Description must not exceed 100 characters'),
+    price: Yup.number().required('Price is required'),
+    quantityProducts: Yup.number().required('Quantity is required'),
+});
+
+const EditItem = ({ item, updateItem, fetchItemById }) => {
+
+  const [Product, setProduct] = useState({});
   let { id } = useParams();
-
   let history = useHistory();
-  const [open, setOpen] = React.useState(false);
-  const [PreviewImage, setPreviewImage] = React.useState();
+  const [open, setOpen] = useState(false);
+  const [PreviewImage, setPreviewImage] = useState();
 
-  const handleToggle = () => {
-    setOpen((prev) => !prev);
-  };
+  const { handleSubmit, control } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
 
-  React.useEffect(() => {
-    if (alert.length === 1) {
-      setOpen(false);
-    }
-  }, []);
-
-  const uploadedImage = useRef();
   const classes = useStyles();
-  const handleImageUpload = (e) => {
-    const [file] = e.target.files;
-    if (file) {
-      const reader = new FileReader();
-      const { current } = uploadedImage;
-      current.file = file;
-      reader.onload = (e) => {
-        setPreviewImage(reader.result);
-        setProduct({ ...Product, imageUrl: file });
-      };
-
-      reader.readAsDataURL(file);
-    }
-  };
-  const handleChange = (e) => {
-    setProduct({ ...Product, [e.target.name]: e.target.value });
-  };
 
   useEffect(() => {
     fetchItemById(id);
-
-    setProduct((prevState) => ({
-      ...prevState,
-      id: item.item._id,
-      title: item.item.title,
-      description: item.item.description,
-      price: item.item.price,
-      imageUrl: item.item.imageUrl,
-      quantityProducts: item.item.quantityProducts,
-      gender: item.item.gender,
-    }));
+      setProduct((prevState) => ({
+        ...prevState,
+        id: item.item._id,
+        title: item.item.title,
+        description: item.item.description,
+        price: item.item.price,
+        imageUrl: item.item.imageUrl,
+        quantityProducts: item.item.quantityProducts,
+        gender: item.item.gender,
+      }));
   }, [
     fetchItemById,
     id,
@@ -122,28 +107,53 @@ const EditItem = ({ item, updateItem, userId, fetchItemById }) => {
     item.item.title,
     item.item.gender,
   ]);
-  const handleSubmit = (e) => {
-    e.preventDefault();
+
+  const OnSubmit = (data) => {
+console.log(data)
+
     const {
       title,
       description,
       price,
       quantityProducts,
-      imageUrl,
-      id,
-      gender,
-    } = Product;
+      gender
+    } = data;
+
     let formData = new FormData();
     formData.append('title', title);
     formData.append('id', id);
     formData.append('description', description);
-    formData.append('image', imageUrl);
+    formData.append('image', Product.imageUrl);
     formData.append('quantity', quantityProducts);
     formData.append('price', price);
     formData.append('gender', gender);
     updateItem(id, formData, history);
   };
-  const { title, description, price, quantityProducts, gender } = Product;
+
+
+  const handleToggle = () => {
+    setOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (alert.length >= 1) {
+      setOpen(false);
+    };
+  }, []);
+
+
+  const handleChange = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPreviewImage(reader.result);
+        setProduct({ ...Product, imageUrl: file });
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
   if (!item.isLoaded) {
     return (
       <Container component='main' maxWidth='md'>
@@ -155,19 +165,14 @@ const EditItem = ({ item, updateItem, userId, fetchItemById }) => {
     <Container component='main' maxWidth='md'>
       <Titles> Edit Product</Titles>
       <CssBaseline />
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr',
-        }}
-      >
-        <div
-          style={{
-            display: 'grid',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '500px',
-          }}
+      <Grid container spacing={5}>
+      <Grid item xs={12} sm={6}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={3}
         >
           {Product.imageUrl && (
             <img
@@ -178,104 +183,148 @@ const EditItem = ({ item, updateItem, userId, fetchItemById }) => {
               }}
             />
           )}
-        </div>
+        </Grid>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <form className={classes.form} noValidate
+          onSubmit={handleSubmit(OnSubmit)}
+          >
+            <FormLabel component='legend'>Collections:</FormLabel>
+            <Controller
+                    rules={{ required: true }}
+                    control={control}
+                    defaultValue={item.item.gender}
+                    name='gender'
+                    render={({ field }) => (
+                      <RadioGroup {...field}>
+                        <FormControlLabel
+                       value='women'
+                       margin='normal'
+                       control={<Radio />}
+                       label='Women'
+                        />
+                        <FormControlLabel
+                          value='men' control={<Radio />} label='Men'
+                        />
+                        <FormControlLabel
+                         value='sneakers'
+                         control={<Radio />}
+                         label='Sneakers'
+                        />
+                      </RadioGroup>
+                    )}
+                  />
 
-        <div>
-          <form className={classes.form} noValidate onSubmit={handleSubmit}>
-            <FormLabel component='legend'>For:</FormLabel>
-            <RadioGroup
-              aria-label='gender'
-              name='gender'
-              value={Product.gender ? Product.gender : ''}
-              onChange={handleChange}
-            >
-              <FormControlLabel
-                value='women'
-                control={<Radio />}
-                label='Women'
-              />
-              <FormControlLabel value='men' control={<Radio />} label='Men' />
-              <FormControlLabel
-                value='sneakers'
-                control={<Radio />}
-                label='Sneakers'
-              />
-            </RadioGroup>
-            <TextField
-              variant='outlined'
-              margin='normal'
-              required={true}
-              fullWidth
-              id='title'
-              label='Title'
-              name='title'
-              autoComplete='title'
-              value={Product.title}
-              onChange={handleChange}
-              defaultValue={title}
-            />
-            <TextField
-              variant='outlined'
-              margin='normal'
-              required={true}
-              fullWidth
-              name='description'
-              label='Description'
-              id='description'
-              autoComplete='description'
-              value={Product.description}
-              onChange={handleChange}
-              defaultValue={description}
-            />
+            <Controller
+              name="title"
+              control={control}
+              defaultValue={item.item.title}
+              render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <TextField
+                  label="Title"
+                  margin='normal'
+                  variant='outlined'
+                  fullWidth
+                  onChange={onChange}
+                  value={value}
+                  error={!!error}
+                  helperText={error ? error.message : null}
 
-            <div style={{ display: 'flex', gap: '30px' }}>
-              <input
+                />
+              )}
+              rules={{ required: 'Title is required' }} />
+              <Controller
+              name="description"
+              control={control}
+              defaultValue={item.item.description}
+              render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <TextField
+                  label="Description"
+                  margin='normal'
+                  variant='outlined'
+                  fullWidth
+                  onChange={onChange}
+                  value={value}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+
+                />
+              )}
+              rules={{ required: 'Description is required' }} />
+
+
+
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                >
+                <TextField
                 margin='normal'
-                required={true}
-                type='file'
+                 type='file'
                 accept='image/*'
-                name='imageUrl'
+                name='file'
                 label='Add image'
                 multiple={false}
-                id='imageUrl'
+                id='file'
                 autoComplete='imageUrl'
-                ref={uploadedImage}
+                onChange={handleChange}
                 className={classes.input}
-                onChange={handleImageUpload}
+
               />
-              <label htmlFor='imageUrl'>
+              <label htmlFor='file'>
                 <Button variant='contained' color='primary' component='span'>
                   Upload
                 </Button>
-              </label>
-            </div>
-            <TextField
-              variant='outlined'
-              margin='normal'
-              required={true}
-              fullWidth
-              name='price'
-              label='Price'
-              id='price'
-              autoComplete='price'
-              value={Product.price}
-              onChange={handleChange}
-              defaultValue={price}
-            />
 
-            <TextField
-              variant='outlined'
-              margin='normal'
-              required={true}
-              fullWidth
-              id='quantityProducts'
-              label='Quantity'
-              name='quantityProducts'
-              autoComplete='quantityProducts'
-              value={Product.quantityProducts}
-              onChange={handleChange}
-              defaultValue={quantityProducts}
-            />
+              </label>
+              <Button variant='contained' color='primary' component='span' >reset</Button>
+              </Grid>
+
+
+            <Controller
+              name="price"
+              control={control}
+              defaultValue={item.item.price}
+              render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <TextField
+                variant='outlined'
+                margin='normal'
+                fullWidth
+                name='price'
+                label='Price'
+                id='price'
+                autoComplete='price'
+                  onChange={onChange}
+                  value={value}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: 'Price is required' }} />
+
+              <Controller
+              name="quantityProducts"
+              control={control}
+              defaultValue={item.item.quantityProducts}
+              render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <TextField
+                variant='outlined'
+                margin='normal'
+                fullWidth
+                name='quantity'
+                label='Quantity'
+                id='price'
+                autoComplete='quantity'
+                  onChange={onChange}
+                  value={value}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: 'Quantity is required' }} />
+
             <div className={classes.containerSubmit}>
               <Button
                 type='submit'
@@ -287,14 +336,14 @@ const EditItem = ({ item, updateItem, userId, fetchItemById }) => {
               >
                 {!open ? (
                   'Submit'
-                ) : (
-                  <CircularProgress color='inherit' thickness={2.3} size={30} />
+                  ) : (
+                <CircularProgress color='inherit' thickness={2.3} size={30} />
                 )}
               </Button>
             </div>
           </form>
-        </div>
-      </div>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
