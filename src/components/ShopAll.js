1cad6291f6e69d97ca8a7d73c2b3fac2f5b5837f
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-29 16:17:43 -0400

    Add styles center

--- src/components/ShopAll.js
+++ src/components/ShopAll.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, {useState}from 'react';
 import Grid from '@material-ui/core/Grid';
 import { useSelector } from 'react-redux';
 import ListItems from './ListItems';
 import { CircularProgress } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ModalItemDetails from './ModalItemDetails';
+
+const useStyles = makeStyles((theme) => ({
+center : {
+  display: 'flex',
+  justifyContent: 'center',
+}
+}));
 
 function ShopAll() {
+
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [idItem, setIdItem]= useState('');
+
   const { itemsPerPages, loading } = useSelector((state) => ({
     itemsPerPages: state.pages.itemsPerPages,
     loading: state.pages.isLoadingPages,
   }));
 
+  const handleClickOpen = (id) => {
+    setOpen(true);
+    if(id){
+  setIdItem(id);}
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   if (!loading) {
     return (
       <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-        }}
+      className={classes.center}
       >
         <CircularProgress />
       </div>
@@ -33,9 +53,15 @@ function ShopAll() {
               title={data.title}
               image={data.imageUrl}
               description={data.description}
+              handleClickOpen={handleClickOpen}
             />
           ))}
       </Grid>
+      <ModalItemDetails
+        open={open}
+        handleClose={handleClose}
+         id={idItem}
+       />
     </div>
   );
 }
